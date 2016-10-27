b3706b0716f07c68ca871b5bd8649c1283a0c81b
alsash <alsashcom@gmail.com>
2016-10-27 17:37:49 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/bignerdranch_photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.bignerdranch_photogallery;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mRecyclerView = (RecyclerView) rootView
+                .findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return rootView;
+    }
+
+}
